e9972c3137dd21cdcb130fa806ae6cdd8343840b
sofiane <djefel.sofiane@gmail.com>
2024-10-30 16:37:52 +0100

    first commit

---
+++ cypress/e2e/exo1.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+describe("Autentification nominale", function () {
+  it("connexion Ok", function () {
+    cy.visit("https://www.saucedemo.com/");
+    cy.get('[data-test="username"]')
+      .type("standard_user")
+      .should("have.value", "standard_user");
+    cy.get('[data-test="password"]')
+      .type("secret_sauce")
+      .should("have.value", "secret_sauce");
+    cy.get('[data-test="login-button"]').click();
+    cy.url().should("include", "/inventory.html");
+    cy.get('[data-test="inventory-item-name"]').should("be.visible");
+    c;
+  });
+});
+
+describe("Autentification non passant", function () {
+  it("connexion KO", function () {
+    cy.visit("https://www.saucedemo.com/");
+    cy.get('[data-test="username"]').type("standard_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
+    cy.get('[data-test="login-button"]').click();
+  });
+});
---
+++ cypress/e2e/exo2.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+describe("Autentification nominale", function () {
+  it("connexion Ok", function () {
+    cy.visit("https://practice.automationtesteracademy.com/");
+    cy.get('[data-test="username-login"]')
+      .type("login_user")
+      .should("have.value", "login_user");
+    cy.get('[data-test="password-login"]')
+      .type("cypress-geek")
+      .should("have.value", "cypress-geek");
+    cy.get('[data-test="remember-login"]').check().should("be.checked");
+    cy.get('[data-test="submit-login"]').click();
+    cy.url().should("include", "/home");
+    cy.get('[data-test="title-product"]').should("be.visible");
+  });
+
+  it("connexion KO", function () {
+    cy.visit("https://practice.automationtesteracademy.com/");
+    cy.get('[data-test="username-login"]')
+      .type("geek_user")
+      .should("have.value", "geek_user");
+    cy.get('[data-test="password-login"]')
+      .type("cypress_geek")
+      .should("have.value", "cypress_geek");
+    cy.get('[data-test="submit-login"]').click();
+    cy.get('[class="error-message"]')
+      .should("be.visible")
+      .and(
+        "contain",
+        "Veuillez vérifier votre nom d'utilisateur ou mot de passe"
+      );
+  });
+});
