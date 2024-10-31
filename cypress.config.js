ac822198a6a6a95643ca26b620b0a3152c797b37
Alee <monarc1937@gmail.com>
2024-10-31 16:13:37 +0100

    use cypress custom commands

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     chromeWebSecurity: false,
-
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "https://www.saucedemo.com",
   },
 });
--- cypress/e2e/exo3.cy.js
+++ cypress/e2e/exo3.cy.js
@@ -1,32 +1,32 @@
 /// <reference types="cypress" />
-describe("Autentification nominale", function () {
-  it("connexion Ok", function () {
-    cy.visit("https://www.saucedemo.com/");
-    cy.get('[data-test="username"]')
-      .type("standard_user")
-      .should("have.value", "standard_user");
-    cy.get('[data-test="password"]')
-      .type("secret_sauce")
-      .should("have.value", "secret_sauce");
-    cy.get('[data-test="login-button"]').click();
+describe("Achats de produits ", function () {
+  it("achat complet de produit Cartons", function () {
+    // Authentification
+    cy.visit("/");
+
+    cy.login("standard_user", "secret_sauce");
+    // Achat de produits
     cy.url().should("include", "/inventory.html");
     cy.get('[data-test="inventory-item-name"]').should("be.visible");
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+    // Validation du panier
     cy.get('[data-test="shopping-cart-badge"]')
       .should("have.text", "1")
       .click();
     cy.get('[data-test="checkout"]').click();
-    cy.get('[data-test="firstName"]')
-      .type("Sofiane")
-      .should("have.value", "Sofiane");
-    cy.get('[data-test="lastName"]').type("Adad").should("have.value", "Adad");
-    cy.get('[data-test="postalCode"]')
-      .type("95100")
-      .should("have.value", "95100");
-    cy.get('[data-test="continue"]').click();
+
+    // Informations d'utilisateurs
+    cy.userForm("Nabil", "Barka", "92000");
+
+    // Confirmation de commande
     cy.url().should("include", "/checkout-step-two.html");
     cy.get('[data-test="finish"]').click();
     cy.contains("Thank you for your order!");
     cy.get('[data-test="back-to-products"]').click();
   });
+
+  it("achats de produits masque", () => {});
+
+  it("achat à partir du burger menu", () => {});
 });
